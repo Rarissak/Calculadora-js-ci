4f320c6cde9113557dce5b670143cd376e0652e1
Rarissak <124792677+Rarissak@users.noreply.github.com>
2023-10-31 09:31:56 -0300

    chore: update test

--- calculadora.test.js
+++ calculadora.test.js
@@ -5,7 +5,7 @@ describe('Teste da calculadora', () => {
         expect(somar(2,3)).toBe(5)
         expect(somar(-1,-1)).toBe(-2)
         expect(somar(2.5,2.25)).toBe(4.75)
-        expect(somar(10,0)).toBe(10)
+        expect(somar(10,0)).toBe(0)
     });
 });
 
